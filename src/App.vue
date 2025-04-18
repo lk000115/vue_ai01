494a79069a91721f28b12cca82e8edcbc89a8f41
like115 <258861342@qq.com>
2025-04-18 16:50:44 +0800

    sd

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import demo from './example/demo.vue'
+import Demo1 from './example/demo1.vue'
 </script>
 
 <template>
 
   <HelloWorld msg="Vite + Vue" />
-  <demo />
+  <Demo1 />
 </template>
 
 <style scoped>
---
+++ src/example/demo1.vue
@@ -0,0 +1,29 @@
+<template>
+    <div >
+        <h1>事件处理</h1>
+        {{ count }}
+</div>
+<div>
+    <button @click="handleClick(100)">add</button>
+</div>
+
+<div>
+    <input type="text" @input="handleInput">
+</div>
+</template>
+
+<script   setup>
+import { ref } from 'vue'
+const count = ref(0)
+const handleClick = (num) => {
+    count.value += num
+}
+const handleInput = (e) => {
+    console.log(e.target.value)
+}
+
+</script>
+
+
+<style scoped>
+</style> 
