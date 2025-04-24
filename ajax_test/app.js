7c91277f176dafe84b50fdeebe9e9d8808c0ae13
like115 <258861342@qq.com>
2025-04-24 17:00:22 +0800

    sd

--- ajax_test/app.js
+++ ajax_test/app.js
@@ -1,11 +1,19 @@
 //引入express模块,创建服务器
 const express = require('express');
 
-const path = require("path");
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/first', (req, res) => {
+  res.send({"name":"zhangsan"});
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
