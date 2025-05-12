f12a3ed59049c8552922ad0da61d74dc65973f5c
like115 <258861342@qq.com>
2025-05-12 13:55:23 +0800

    sd

--- ajax_test/public/js/测试.js
+++ ajax_test/public/js/测试.js
@@ -1,4 +1,4 @@
-
+import axios from 'axios'
 function fn(obj){
     var default1 = {
         name:'zs',
@@ -16,8 +16,16 @@ function fn(obj){
 // fn({name:'dddddd',age:20,sex:"",success:(res)=>{console.log(res);
 // }})
 
+//async修饰的函数包裹的代码,其中异步代码就像同步代码一样执行
 async  function test() {
-   await console.log(11111);   
+await 后面是一个promise对象,会等待promise对象执行完成后再执行后面的代码,注意只能是promise对象,不能是普通对象    
+  await new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log(1111);
+            resolve()
+        },1000)
+    }) 
    console.log(2222);
+
 }
 test()
