787345b877f291373934cb1925478de924778c15
like115 <258861342@qq.com>
2025-05-20 17:12:26 +0800

    sd

---
+++ ajax_test/app02.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use("/db",require("./public/js/messRouter"));
+
+app.get("/",(req, res,)=>{
+    res.send("hello world");
+})
+
+
+
+
+app.listen(port, () => {
+
+    console.log(`服务器已经运行成功: http://localhost:${port}`);
+})
