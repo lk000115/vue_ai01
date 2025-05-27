2731e510c87fc89e6bd37281b4dd1bba6a0ed96f
like115 <258861342@qq.com>
2025-05-27 17:22:16 +0800

    sd

--- blog/server/app.js
+++ blog/server/app.js
@@ -1,6 +1,21 @@
 const express  = require("express");
 const app = express();
+const cors = require("cors");
+const multer = require("multer");
 const port = 3000;
+const path = require("path");
+
+//处理跨域
+app.use(cors());
+//处理Json数据
+app.use(express.json());
+
+//把上传的文件存放此临时目录
+const upload = multer({ dest: './public/upload/temp' });
+app.use(upload.any());
+
+
+
 // app.use(express.static())
 
 app.get("/", (req, res)=> {
