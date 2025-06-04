faf1326710f14b29205c2147f69c4c0355d247a6
like115 <258861342@qq.com>
2025-06-04 17:22:20 +0800

    sd

--- blog/server/app.js
+++ blog/server/app.js
@@ -13,13 +13,14 @@ app.use(express.json());
 //把上传的文件存放此临时目录
 const upload = multer({ dest: './public/upload/temp' });
 app.use(upload.any());
-
-
+//设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/db', require('./routers/TestRouter'));
 app.use("/admin", require('./routers/AdminRouter'))
 app.use("/category", require('./routers/CategoryRouter'))
 app.use("/blog", require("./routers/BlogRouter"));
+app.use("/upload", require("./routers/UploadRouter"));
 
 
 app.get("/", (req, res)=> {
--- blog/server/routers/UploadRouter.js
+++ blog/server/routers/UploadRouter.js
@@ -5,7 +5,7 @@ const {db,genid} = require("../db/DbUtils");
 const fs = require("fs");
 
 
-router.post("/upload", async (req, res)=>{
+router.post("/refile", async (req, res)=>{
     console.log(req.files)
     if (!req.files) {
         res.send({
@@ -23,7 +23,7 @@ router.post("/upload", async (req, res)=>{
          //获取文件名后缀
          let file_ext = file.originalname.substring(file.originalname.lastIndexOf('.') + 1 );
          //使用时间戳为文件名称
-         let file_name = new Date().getTime() + '.' + file_ext;
+         let file_name = genid.NextId() + '.' + file_ext;
          //保存文件到指定目录
          fs.renameSync(
             process.cwd() + '/public/upload/temp/' + file.filename,
@@ -32,6 +32,13 @@ router.post("/upload", async (req, res)=>{
          ret_files.push("/public/upload/" + file_name);
       } 
       
+      res.send({
+        "errno": 0, 
+        "data":{
+          "url": ret_files[0] 
+        }
+    
+    })
 
 })
 
