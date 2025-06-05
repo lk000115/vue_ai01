bd5c742a98ecddc8abad06757e2a6b7ea504c7d2
like115 <258861342@qq.com>
2025-06-05 11:22:53 +0800

    sd

--- blog/client/src/main.js
+++ blog/client/src/main.js
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+/**
+ * 需要安装的插件
+ * axios  pinia  sass 
+ * vue-router   npm install vue-router@4
+ * naive-ui  框架  npm i -D naive-ui
+ * npm i -D vfonts  字体
+ * wangEditor 富文本编辑器    npm install @wangeditor/editor-for-vue@next --save
+ */
+
+
+
+
+
+
 createApp(App).mount('#app')
