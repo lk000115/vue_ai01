7d5d4d4c475e06554e0889bcb1b97fc3d473037a
like115 <258861342@qq.com>
2025-06-11 17:00:16 +0800

    sd

--- blog/client/src/views/dashboard/Dashboard.vue
+++ blog/client/src/views/dashboard/Dashboard.vue
@@ -1,12 +1,61 @@
 <template>
-<div >
-    <h1>Welcome to Dashboard</h1>
+<div class="main-panel">
+     <div class="menus">
+        <div v-for="(menu, index) in menus" :key="index" @click="router.push(menu.href)">
+            {{ menu.name }}
+            
+        </div>
+         
+     </div> 
 </div>
 </template>
 
 
 <script   setup>
+import { AdminStore } from '../../stores/AdminStore';
+import{ref,inject} from 'vue';
+import { useRouter,useRoute } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+
+const adminStore = AdminStore();
+const axios = inject('axios');
+const message = inject('message');
+
+let menus = [
+    {name:'文章管理',href:'/dashboard/article'},
+    {name:'分类管理',href:'/dashboard/category'},
+    {name:'退出',href:'/dashboard/logout'},
+]
 
 </script>
-<style scoped>
+
+
+
+<style lang="scss" scoped>
+.main-panel {
+    display: flex;
+    color:#64676a;
+    max-width: 1500px;
+    margin: 0 auto;
+}
+
+.menus {
+    width: 100px;
+    height: 95vh;
+    padding: 20px, 0;
+    box-sizing:border-box;
+    border-right: 1px solid #dadada;
+    text-align: center;
+    line-height: 55px;
+
+    div {
+        cursor: pointer;
+        &:hover {
+            color: #fd760e;}  
+           
+    }
+
+}
 </style> 
