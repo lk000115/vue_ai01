f3bf4cfbf1f22d5a5e1f53507e5f69bc2f0935cf
like115 <258861342@qq.com>
2025-07-03 11:22:38 +0800

    sd

--- invManagement/server/routers/InvRouter.js
+++ invManagement/server/routers/InvRouter.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const db = require("../db/DbUtils");
 
 
+router.get('/list',async (req, res)=>{
+    let sql = "select * from invList"
+    let {err,rows} = await db.async.all(sql)
+    if(err == null) {
+        res.send({code: 200, msg: "查询成功",data:rows})
+    }else{
+        res.send({code: 500, msg: "查询失败"})
+    }
+})
 
 
 
