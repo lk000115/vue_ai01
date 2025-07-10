57cfb7ceeed6dea635eeafee49daa4c153e01fd3
like115 <258861342@qq.com>
2025-07-10 16:05:10 +0800

    sd

--- invManagement/client/vite.config.js
+++ invManagement/client/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, '192.168.1.190+3-key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, '192.168.1.190+3.pem'))
+      }
+    },
+    port: '443',
+    host:'127.0.0.1',
 })
